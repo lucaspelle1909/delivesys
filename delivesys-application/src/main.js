f830211ad28cc3b786cafb364da285503d5aa198
yTchupaCabra <tibika10@hotmail.com>
2021-07-07 22:24:17 -0300

    pre login

--- delivesys-application/src/main.js
+++ delivesys-application/src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import vuetify from '@/config/vuetify.js'
+import router from '@/config/router.js'
+import store from '@/store/store.js'
+
+import '@/config/axios.js'
 
 Vue.config.productionTip = false
 
 new Vue({
+  vuetify,
   router,
   store,
   render: h => h(App)
